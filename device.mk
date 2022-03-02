1ea6635710221a0e926a1c1f2a59920886c017a0
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-03-02 06:09:08 -0600

    mido: build radio.config@1.0 packages
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- device.mk
+++ device.mk
@@ -404,6 +404,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.0.vendor \
     android.hardware.radio.config@1.2 \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0 \
