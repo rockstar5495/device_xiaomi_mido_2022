dfbc901ee5e01d9d768605349dcec36d326d499c
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-08-03 15:11:56 +0530

    mido: drop fp&desc
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- octavi_mido.mk
+++ octavi_mido.mk
@@ -42,9 +42,3 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
